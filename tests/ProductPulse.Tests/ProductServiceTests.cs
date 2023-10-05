8898e25a1c077a0f8da0cc6eafdebec235d0360e
Renan Barbosa <renanvinicius.quadros@gmail.com>
2023-10-05 20:02:17 -0300

    fix test

--- tests/ProductPulse.Tests/ProductServiceTests.cs
+++ tests/ProductPulse.Tests/ProductServiceTests.cs
@@ -43,7 +43,7 @@ namespace ProductPulse.Tests
 
             unitOfWorkMock.Setup(u => u.Products.GetById(productId)).ReturnsAsync(productToDelete);
             unitOfWorkMock.Setup(u => u.Products.Delete(It.IsAny<Product>()));
-            unitOfWorkMock.Setup(u => u.Save()).Returns(0);
+            unitOfWorkMock.Setup(u => u.Save()).Returns(1);
 
             // Act
             var result = await productService.DeleteProduct(productId);
